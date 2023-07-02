fb6cf326939a66eb41a67c8f6973e5ce3cc3a9f3
AnyiC <anyichen212@gmail.com>
2023-07-02 14:46:01 -0400

    GET single student by id working

--- api/student.js
+++ api/student.js
@@ -4,7 +4,7 @@ const express = require("express");
 const router = express.Router();
 const { Student } = require("../db/models");
 
-//api/student
+//localhost:8080/api/student
 
 //get all students
 router.get("/", async (req, res) => {
@@ -17,6 +17,23 @@ router.get("/", async (req, res) => {
     } catch (error) {
         console.log("GET all Student error: ", error);
     }
+});
+
+//get single student by id
+router.get("/:id", async (req,res) => {
+    const id = req.params.id;
+
+    try {
+        console.log("On Student id : ", id, ", api");
+        const student = await Student.findByPk(id);
+
+        student
+            ? res.status(200).json(student)
+            : res.status(400).send(`Campus id, ${id}, is NOT found. `);
+        
+    } catch (error) {
+        console.log(error);
+    };
 });
 
 module.exports = router;
