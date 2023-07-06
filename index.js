697694f89d2198a6e65673e7af024909aa255559
AnyiC <anyichen212@gmail.com>
2023-07-06 14:42:40 -0400

    delete unuse test code

--- index.js
+++ index.js
@@ -6,25 +6,6 @@ require('dotenv').config();
 
 const app = express();
 
-// // To upload image to server
-// const cloudinary = require("cloudinary").v2;
-
-// // To extract data from incoming request
-// const bodyParser = require('body-parser');
-
-// // To get file paths, remove files
-// const fs = require('fs')
-
-// // body parser configuration
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// cloudinary.config({
-//     cloud_name: process.env.CLOUDINARY_NAME,
-//     api_key: process.env.CLOUDINARY_API_KEY,
-//     api_secret: process.env.CLOUDINARY_API_SECRET
-// });
-
 app.use(express.json());
 app.use(cors());
 
